99244656ac81d31edc411ba91c0d8f22ee85613c
sowebsite <soeduwebsite@gmail.com>
2014-11-09 19:46:13 -0500

    First Release
    First Release after Remove DevExpress

---
+++ StepOneEducation_0_6/Common/Email.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+
+namespace Common
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public void sendFormEmail(string name, string emailfrom, string comment, string textIntxtArea, string essayType, HttpPostedFileBase essaySubmit)
+        {
+            string from = "soeduwebsite@gmail.com";
+            string To = "soeduwebsite@gmail.com";
+            //string textIntxtArea = form["txtEssay"].ToString();
+            //string essayType = "作文类型为：" + form["essayType"].ToString() + "\t";
+
+
+            using (MailMessage mail = new MailMessage(emailfrom, To))
+            {
+                //mail.Subject = objModelMail.Subject;
+                //mail.Body = objModelMail.Body;                        
+
+                mail.Subject = "作文修改  来自" + name + " " + emailfrom;
+                mail.Body = essayType + "\t请批改作业\n\n" + comment + "\n\n";
+
+                if (essaySubmit != null)
+                {
+                    string fileName = Path.GetFileName(essaySubmit.FileName);
+                    mail.Attachments.Add(new Attachment(essaySubmit.InputStream, fileName));
+                }
+                else
+                {
+                    mail.Body += textIntxtArea;
+                }
+
+                mail.IsBodyHtml = false;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                NetworkCredential nc = new NetworkCredential(from, "Hope4321");
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = nc;
+                smtp.Port = 587;
+                smtp.Send(mail);
+            }
+        }
+    }
+}
---
+++ StepOneEducation_0_6/StepOneEducation_1_0/Controllers/CaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StepOneEducation_1_0.Controllers
+{
+    public class CaseController : Controller
+    {
+        //
+        // GET: /Case/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
